35e7c25d5e85ea13cad645fe8eca4f80c21f0d1e
Anthony Lowhur <antlowhur@yahoo.com>
2015-10-17 01:10:27 -0400

    First attempt in doing road segmentation

--- road-segment.py
+++ road-segment.py
@@ -1,23 +1,32 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
+import imutils
+
+def invert_img(img):
+    img = (255-img)
+    return img
+
 
 #img = cv2.imread('images/coins_clustered.jpg')
-img = cv2.imread('images/road_0.jpg')
+img = cv2.imread('images/road_4.jpg')
+img = imutils.resize(img, height = 300)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
+thresh = invert_img(thresh)
+
 # noise removal
 kernel = np.ones((3,3), np.uint8)
-opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 4)
 
 # sure background area
 sure_bg = cv2.dilate(opening,kernel,iterations=3)
 
+#sure_bg = cv2.morphologyEx(sure_bg, cv2.MORPH_TOPHAT, kernel)
+
 # Finding sure foreground area
-#dist_transform = cv2.distanceTransform(opening, cv2.cv.CV_DIST_L2, 5)
-# For OpenCV 3.0.0
 dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
 ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
 
@@ -33,9 +42,17 @@ markers = markers+1
 
 # Now, mark the region of unknown with zero
 markers[unknown==255] = 0
-
+'''
+imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgray = cv2.GaussianBlur(imgray, (5, 5), 0)
+img = cv2.Canny(imgray,200,500)
+'''
 markers = cv2.watershed(img,markers)
 img[markers == -1] = [255,0,0]
 
+
+cv2.imshow('background',sure_bg)
+cv2.imshow('foreground',sure_fg)
+cv2.imshow('threshold',thresh)
 cv2.imshow('result',img)
 cv2.waitKey(0)
